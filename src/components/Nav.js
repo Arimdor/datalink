9a617dca96006f4470f946ba43016c92e9295d55
Arimdor <rodrigo.120894@gmail.com>
2018-09-20 13:26:16 -0500

    nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,13 +4,13 @@ import logo from '../logo.svg';
 class Nav extends Component {
     render() {
         return (
-            <nav className="navbar navbar-light bg-light">
+            <nav className="navbar navbar-light bg-dark">
                 <div className="container-fluid">
                     <div className="col-md-1 d-none d-md-inline p-0 m-0"/>
                     <div className="col-12 col-md-11 mx-md-0 px-md-0">
                         <a className="navbar-brand" href="#">
                             <img src={logo} className="App-logo" alt="logo"/>
-                            <span className="text-monospace">DATALINK 718th</span>
+                            <span className="text-monospace text-white">DATALINK 718th</span>
                         </a>
                     </div>
                 </div>
